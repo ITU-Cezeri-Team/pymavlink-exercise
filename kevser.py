de641ba8bb6397249534968a08cdd84b97d47138
cavcer <akdagk24@itu.edu.tr>
2024-12-09 09:15:24 +0300

    prepare code for connection

--- kevser.py
+++ kevser.py
@@ -1,1 +1,16 @@
-selam1
+#190 268 4.161
+
+from pymavlink import mavutil
+
+# Bağlantıyı kur
+master = mavutil.mavlink_connection('192.168.4.161')
+
+# Heartbeat mesajını bekle
+master.wait_heartbeat()
+print("Heartbeat alındı! İHA iletişime hazır.")
+
+# Alınan heartbeat mesajını incele
+heartbeat = master.recv_match(type='HEARTBEAT', blocking=True)
+print(f"Sistem Durumu: {heartbeat.system_status}")
+print(f"Temel Mod: {heartbeat.base_mode}")
+print(f"Özel Mod: {heartbeat.custom_mode}")
