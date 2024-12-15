4a8ea03852057d45858a29a8ee51b26645afdbd8
Feyza_Karapehlivan <feyzakarapehlivan@gmail.com>
2024-12-15 15:50:14 +0300

    helö

--- feyza.py
+++ feyza.py
@@ -1,3 +1,5 @@
+
+
 from pymavlink import mavutil
 
 # Bağlantıyı kur
@@ -7,7 +9,6 @@ print("Connection established")
 arm_control = input("For ARM press Y: ").lower()
 
 if arm_control=="y":
-    master.set_mode(mavutil.mavlink.MAV_MODE_STABİLİZE_ARMED)
     master.arducopter_arm()
     print("Motorlar aktif edildi.")
 else:
@@ -15,7 +16,7 @@ else:
 
 takeoff_control = input("For take off press Y: ").lower()
 
-if takeoff_control=="y":
+if takeoff_control=="y": 
     master.set_mode(mavutil.mavlink.MAV_MODE_GUIDED_ARMED)
     print("Switched to GUIDED mode.")
     takeoff_altitude = 10  # 10 metre
@@ -27,8 +28,6 @@ if takeoff_control=="y":
     0, 0, 0, 0, 0, 0, takeoff_altitude  # kalkış yüksekliği
     )
     print(f"Takeoff command sent to reach {takeoff_altitude} meters.")
-    master.arducopter_disarm()
-    print("Motorlar pasif edildi.")
 
 
 
@@ -37,40 +36,5 @@ if takeoff_control=="y":
 
 
 
-
-
-
-
-"""
-# Heartbeat mesajını bekle
-try:
-    master.wait_heartbeat(timeout=10)  # 10 saniye bekler
-    print("Heartbeat alındı! İHA iletişime hazır.")
-except Exception as e:
-    print(f"Heartbeat alınamadı: {e}")
-    exit()  # Eğer heartbeat alınmazsa programı sonlandır
-"""
-"""
-# Batarya durumunu al
-try:
-    battery = master.recv_match(type='BATTERY_STATUS', blocking=False)
-    print("Battery connected")
-    if battery:
-        print(f"Batarya Voltajı: {battery.voltages[0] / 1000} V")  # mV -> V dönüşümü
-        print(f"Batarya Akımı: {battery.current_battery / 100} A")  # cA -> A dönüşümü
-        print(f"Kalan Batarya: {battery.battery_remaining} %")
-    else:
-        print("Batarya durumu alınamadı.")
-except Exception as e:
-    print(f"Batarya durumu alınamadı: {e}")
-"""
-"""
-# Heartbeat mesajını tekrar al
-try:
-    heartbeat = master.recv_match(type='HEARTBEAT', blocking=True)
-    print(f"Sistem Durumu: {heartbeat.system_status}")
-    print(f"Temel Mod: {heartbeat.base_mode}")
-    print(f"Özel Mod: {heartbeat.custom_mode}")
-except Exception as e:
-    print(f"Heartbeat mesajı alınamadı: {e}")
-"""
+master.arducopter_disarm()
+print("Motorlar pasif edildi.")
