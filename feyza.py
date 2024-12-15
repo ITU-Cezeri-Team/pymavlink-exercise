842b73b38d9d258cc040ba75f098848be22782d5
Feyza_Karapehlivan <feyzakarapehlivan@gmail.com>
2024-12-15 17:23:49 +0300

    5

--- feyza.py
+++ feyza.py
@@ -7,28 +7,26 @@ connection = mavutil.mavlink_connection('/dev/serial0', baud=57600)
 connection.wait_heartbeat()
 print("Heartbeat from system (system %u component %u)" % (connection.target_system, connection.target_component))
 
-# Define command_long_encode message to send MAV_CMD_SET_MESSAGE_INTERVAL command
-# param1: MAVLINK_MSG_ID_BATTERY_STATUS (message to stream)
-# param2: 1000000 (Stream interval in microseconds)
-connection.mav.command_long_send(
-    1,
-    1,
-    mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM,  # ARM komutu
-    0,  # Confirmation
-    1, # param2: Interval in microseconds
-    0,       # param3 (unused)
-    0,       # param4 (unused)
-    0,       # param5 (unused)
-    0,       # param5 (unused)
-    0,        # param6 (unused)
-    0
-    )
+# ARM
+connection.mav.command_long_send(connection.target_system, connection.target_component
+                                 , mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM, 0,1,0,0,0,0,0,0)
 
+msg = connection.recv_match(type="COMMAND_ACK",blocking=True)
+print(msg)
 
+# TAKE OFF
+connection.mav.command_long_send(connection.target_system, connection.target_component
+                                 , mavutil.mavlink.MAV_CMD_NAV_TAKEOFF, 0,0,0,0,0,0,0,10)
 
-# Wait for a response (blocking) to the MAV_CMD_SET_MESSAGE_INTERVAL command and print result
-response = connection.recv_match(type='COMMAND_ACK', blocking=True)
-if response and response.command == mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM and response.result == mavutil.mavlink.MAV_RESULT_ACCEPTED:
-    print("Command accepted")
-else:
-    print("Command failed")
+msg = connection.recv_match(type="COMMAND_ACK",blocking=True)
+print(msg)
+
+connection.mav.send(mavutil.mavlink.MAVLink_set_position_target_local_ned_message(10, connection.target_system, 
+                                                                                  connection.target_component,
+                                                                                    mavutil.mavlink.MAV_FRAME_LOCAL_NED,int(0b110111111000), 10,0,-10,0,0,0,0,0))
+
+while 1:
+    msg = connection.recv_match(
+        type="MAV_CONTROLLER_OUTPUT", blocking=True)
+    print(msg)
+    
