7f479849768f062091c1675e7a46b20570f6251c
Feyza_Karapehlivan <feyzakarapehlivan@gmail.com>
2024-12-15 16:11:15 +0300

    first_try

--- feyza.py
+++ feyza.py
@@ -1,40 +1,8 @@
-
-
 from pymavlink import mavutil
 
-# Bağlantıyı kur
-master = mavutil.mavlink_connection('/dev/serial0', baud=57600)
-print("Connection established")
+# Start a connection listening on a UDP port
+connection = mavutil.mavlink_connection('/dev/serial0', baud=57600)
 
-arm_control = input("For ARM press Y: ").lower()
-
-if arm_control=="y":
-    master.arducopter_arm()
-    print("Motorlar aktif edildi.")
-else:
-    pass 
-
-takeoff_control = input("For take off press Y: ").lower()
-
-if takeoff_control=="y": 
-    master.set_mode(mavutil.mavlink.MAV_MODE_GUIDED_ARMED)
-    print("Switched to GUIDED mode.")
-    takeoff_altitude = 10  # 10 metre
-    master.mav.command_long_send(
-    master.target_system, 
-    master.target_component, 
-    mavutil.mavlink.MAV_CMD_NAV_TAKEOFF, 
-    0,  # confirmation (0: no confirmation needed)
-    0, 0, 0, 0, 0, 0, takeoff_altitude  # kalkış yüksekliği
-    )
-    print(f"Takeoff command sent to reach {takeoff_altitude} meters.")
-
-
-
-
-
-
-
-
-master.arducopter_disarm()
-print("Motorlar pasif edildi.")
+# Wait for the first heartbeat to set the system and component ID of remote system for the link
+connection.wait_heartbeat()
+print("Heartbeat from system (system %u component %u)" % (connection.target_system, connection.target_component))
